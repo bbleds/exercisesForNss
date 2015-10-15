716b83d436c3b82632f9dca5c043fa6d830d7d4c
bbleds <bledsoedesigns@gmail.com>
2015-10-15 14:19:33 -0500

    Doing exercises

--- basic-array-methods/arrays.js
+++ basic-array-methods/arrays.js
@@ -11,19 +11,72 @@
   without the need for a getElementById() and innerHTML.
 */
 
-function reversal(origString) {
 
-}
+/*//  1. document.write() the characters of any string in reverse order. = Done
+			function reversal(origString) {
+				//put origString into array
+				var arrryOfLetters = origString.split("");
+				console.log(arrryOfLetters);
 
-function alphabits() {
+				//reverse array
+				arrryOfLetters.reverse();
+				console.log(arrryOfLetters);
 
-}
+				//join array
+				var reversedText = arrryOfLetters.join("");
+				console.log(reversedText);
+
+				//output new joined string 
+				document.write(reversedText);
+			}
+
+
+// 2. document.write() the characters of any string in alphabetical order.
+
+			function alphabits(theString) {
+				//split string into array
+				var splitText = theString.split("");
+				console.log("My split text is", splitText);
+
+
+				//sort the array letters
+				splitText.sort(function (a, b) {
+    				return a.toLowerCase().localeCompare(b.toLowerCase());
+				});
+				console.log(splitText);
+
+				//join
+				var joinedStr = splitText.join("");
+				console.log("Joined aplpha is", joinedStr);
+				//output alphabetical
+				document.write("<p>"+joinedStr+"</p>");
+			}
+
+//3. document.write() true, or false, if the passed string is a palidrome.
 
 function palindrome() {
 
+	//split string into two array
+	//join first array into string
+
+	//reverse second array
+	//join second array into string
+
+	//for loop for each letter in array1, comepare to item in array2 if they are the same, add to a counter, 
+	//if counter === array.length, we have pallendrome
 }
 
-var testString = "";
+var testString = "This is my new string";
 reversal(testString);
 alphabits(testString);
-palindrome(testString);
+palindrome(testString);*/
+
+
+function (a, b){
+	return a + b;
+}
+
+
+
+
+
