35c1e832055539bc7c9b369a2d9dbe3eb8597950
bbleds <bledsoedesigns@gmail.com>
2015-10-16 09:48:42 -0500

    creating files for pinetree

---
+++ pinetree/pinetree.js
@@ -0,0 +1,24 @@
+/*
+  Create an `tree` function that accepts an integer
+  as an argument. The function should create a pine
+  tree out of asterisks in the browser console. The 
+  height is whatever is passed as an argument to the 
+  function.
+
+  Example output with argument value of 5
+
+            *
+           ***
+          *****
+         *******
+        *********
+
+*/
+
+// Create your function here. Make sure it takes the height argument.
+
+var tree = function(height) {
+
+};
+
+tree(7);
